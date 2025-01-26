cd98d5c949976dc4b300e736edefcd27ec208364
GuilheermeMarques <guiferreira_94@hotmail.com>
2025-01-26 15:16:07 -0300

    [feature] add entities and answer use case with tests

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,29 @@
+export class Slug {
+    public value: string
+
+    constructor(value: string) {
+        this.value = value
+    }
+
+    /**
+     * Receives a string and normalizes it as a slug
+     * 
+     * Examĺe: "An example title" -> "an-example-title"
+     * 
+     * @param text (string) - The text to be converted to a slug
+     */
+
+    static createFromText(text: string) {
+        const slugText = text
+        .normalize("NFKD") // Removes accents and special characters
+        .toLocaleLowerCase() // Converts to lowercase
+        .trim() // Removes spaces at the beginning and end
+        .replace(/\s+/g, '-') // Replaces spaces with hyphens
+        .replace(/[^\w-]+/g, '') // Removes non-alphanumeric characters except hyphens
+        .replace(/--+/g, '-') // Replaces multiple consecutive hyphens with a single one
+        .replace(/^-+|-+$/g, ''); // Removes hyphens at the beginning or end
+
+            return new Slug(slugText)
+    }
+    
+}
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,6 @@
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
+import { AnswersRepository } from "../repositories/answers-repository";
 
 interface AnswerQuestionUseCaseRequest {
     instructorId: string;
@@ -7,8 +9,18 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-    execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-        const answer = new Answer(content);
+    constructor(
+        private answersRepository: AnswersRepository
+    ) {}
+
+    async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+        const answer = Answer.create({
+            content,
+            authorId: new UniqueEntityId(instructorId),
+            questionId: new UniqueEntityId(questionId)
+        });
+
+        await this.answersRepository.create(answer);
         
         return answer
     }
