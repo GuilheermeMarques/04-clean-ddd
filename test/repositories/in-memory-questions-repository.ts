da296623ae8eb93d93158816715be523bbbd0f50
GuilheermeMarques <guiferreira_94@hotmail.com>
2025-03-04 10:33:15 -0300

    [feature] delete a question use case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,6 +4,17 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  async findById(id: string) {
+    const question =
+      this.items.find((item) => item.id.toString() === id) || null
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug) || null
 
@@ -16,5 +27,11 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question): Promise<void> {
     this.items.push(question)
+  }
+
+  async delete(question: Question) {
+    const itemIdenx = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIdenx, 1)
   }
 }
