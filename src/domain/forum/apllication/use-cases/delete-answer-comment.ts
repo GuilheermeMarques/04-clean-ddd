2f6ada8e6b8706663c3c4c9eacde6ce02cdcd764
GuilheermeMarques <guiferreira_94@hotmail.com>
2025-04-14 09:26:02 -0300

    [feature] delete answer comment use case

---
+++ src/domain/forum/apllication/use-cases/delete-answer-comment.ts
@@ -0,0 +1,32 @@
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+
+interface DeleteAnswerCommentUseCaseRequest {
+  authorId: string
+  answerCommentId: string
+}
+
+interface DeleteAnswerCommentUseCaseResponse {}
+
+export class DeleteAnswerCommentUseCase {
+  constructor(private answersCommentsRepository: AnswerCommentsRepository) {}
+
+  async execute({
+    authorId,
+    answerCommentId,
+  }: DeleteAnswerCommentUseCaseRequest): Promise<DeleteAnswerCommentUseCaseResponse> {
+    const answerComment =
+      await this.answersCommentsRepository.findById(answerCommentId)
+
+    if (!answerComment) {
+      throw new Error('Answer comment not found.')
+    }
+
+    if (authorId !== answerComment.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    await this.answersCommentsRepository.delete(answerComment)
+
+    return {}
+  }
+}
