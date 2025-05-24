231b36ef67041a4361342cb0be6c38f939c5f8c3
GuilheermeMarques <guiferreira_94@hotmail.com>
2025-05-23 21:55:13 -0300

    feat: Domain Events

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,3 +1,29 @@
 import { Entity } from '@/core/entities/entity'
+import { DomainEvent } from '../events/domain-event'
+import { DomainEvents } from '@/core/events/domain-events'
 
-export abstract class AggregateRoot<Props> extends Entity<Props> {}
+export abstract class AggregateRoot<Props> extends Entity<Props> {
+  private _domainEvents: DomainEvent[] = []
+
+  get domainEvents(): DomainEvent[] {
+    return this._domainEvents
+  }
+
+  protected addDomainEvent(domainEvent: DomainEvent): void {
+    this.domainEvents.push(domainEvent)
+    DomainEvents.markAggregateForDispatch(this)
+  }
+
+  public clearEvents(): void {
+    this._domainEvents.splice(0, this._domainEvents.length)
+  }
+
+  public getUncommittedEvents(): DomainEvent[] {
+    return this.domainEvents.filter((event) => !event.ocurredAt)
+  }
+
+  public commit(): void {
+    this.clearEvents()
+    DomainEvents.dispatchEventsForAggregate(this.id)
+  }
+}
